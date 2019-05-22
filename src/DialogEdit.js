89a9625e588bf3e6a05386b38b4f9c70f2361c5e
Mathieu <mathieu@techexmachina.com>
2019-05-22 11:31:33 +0200

    initial commit

--- src/DialogEdit.js
+++ src/DialogEdit.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { AutoForm } from 'uniforms-material'
 
-import Dialog from '@material-ui/core/Dialog'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogActions from '@material-ui/core/DialogActions'
+import Dialog from '@material-ui/core/Dialog/index'
+import DialogTitle from '@material-ui/core/DialogTitle/index'
+import DialogContent from '@material-ui/core/DialogContent/index'
+import DialogActions from '@material-ui/core/DialogActions/index'
 
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button/index'
 
 class DialogEdit extends Component {
   formRef = React.createRef()
